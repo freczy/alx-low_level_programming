af564ec634d77aad40923bc2d84e979f0907884c
freczy <akinyelesamuel26@gmail.com>
2023-03-17 15:42:09 -0700

    m

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,10 +3,10 @@
 #include <time.h>
 
 /**
- *main -> assign a random number to the variable n each time it is executed
- *and print the last digit of the number stored in the variable n
- *Return: Always 0 (Success)
+ *main -> assign a random
+ *Return: Always 0
  */
+
 int main(void)
 
 {
