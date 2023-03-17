8a8559d420f8c5707b17f12e2cf841109a8b8e9c
freczy <akinyelesamuel26@gmail.com>
2023-03-17 14:20:31 -0700

    m

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *and print the last digit of the number stored in the variable n
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	int p;
+
+	for (d = 0, d < 9; d++)
+	{
+		for (p = d + 1; d + 1; p++)
+	{
+		if (p != d)
+	{
+		putchar(d);
+		putchar(p);
+	if (d == 0 && p == 9)
+	continue;
+	putchar(',');
+	putchar(',');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
