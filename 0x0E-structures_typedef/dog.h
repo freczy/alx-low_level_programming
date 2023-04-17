191bdd38cd2e01e823b52099380c2cc1eb798963
freczy <akinyelesamuel26@gmail.com>
2023-04-17 14:34:13 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,20 @@
-#ifndef DOG
-#define DOG
+#include "dog.h"
 
 /**
- * struct dog - dog class
+ * init_dog - initializes a structure of type dog
+ * @d: pointer to structure
+* @name: pointer to the name of the dog
+ * @age: age of dog
+ * @owner: pointer to owner of dog
  *
- * @name: Name of the dog
- *
- * @age: Age of the dog
- *
- * @owner: Owner of the dog
+ * Return: void
  */
-
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-
-void init_dog (struct dog *d, char name, float age, char *owner);
-void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
-
-dog_t *new_dog(char *name, float age, char *owner);
-
-void free_dog(dog_t *d);
-
-#endif
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
