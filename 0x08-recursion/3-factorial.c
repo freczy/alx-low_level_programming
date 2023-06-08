e096365f610cf8327792a8f33327dd4fcecce052
freczy <akinyelesamuel26@gmail.com>
2023-06-08 12:15:14 +0100

    all changes

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,15 +3,15 @@
  * factorial - this is factorial of int
  * @n:integer
  * Return:returns int
- *
  */
 int factorial(int n)
 {
 	int x;
-	if	(n==0)
+	if (n == 0)
 		return (1);
 	else if	(n < 0)
 		return (-1);
-	else x=n*factorial (n-1);
-	return	(x);
+	else
+		x = n * factorial (n - 1);
+	return (x);
 }
